c21c13b3a76fd2c2ea6553e8cd91c6c10d4d386a
c.maiza <carlos.maiza@hotmail.com>
2021-01-04 00:37:18 +0100

    Change minor issues

--- bankist/scripts.js
+++ bankist/scripts.js
@@ -4,28 +4,28 @@
 
 // Data
 const account1 = {
-  owner: 'Konzo Maiza',
+  owner: 'Muana Mola',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'KaLonji Kidus Maiza',
+  owner: 'Bolingo Lingo',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Carlos Maiza',
+  owner: 'Nzinga Mpesa',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Nzolani Maiza',
+  owner: 'Nzola Bikua',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
